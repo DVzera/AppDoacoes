63efa6066ab99f3ec244454ca46bb12819c15de5
DVzera <davicollescaini@gmail.com>
2024-06-18 17:18:18 -0300

    ssds

--- screens/login.js
+++ screens/login.js
@@ -5,6 +5,10 @@ export default function Login({navigation}){
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
+  function Cadastrar(){
+    navigation.navigate("Cadastro")
+  }
+
   function SignIn(){
     if (username == '1' && password == '2'){
       navigation.navigate("Home")
@@ -19,24 +23,28 @@ export default function Login({navigation}){
 
     return(
         <View style={styles.container}>
-            <Text style={styles.text}>Login!!</Text>
-            <Image style={styles.logo} source={{uri:'https://media1.tenor.com/m/ZuXnTDxIbjQAAAAC/shocked-shocked-cat.gif'}}/>
+            <Text style={styles.text}>App Doações</Text>
+            <Text style={styles.text2}>Login</Text>
+            <Text style={styles.text3}>Insira seu login para continuar</Text>
             <TextInput
             style= {styles.input}
-            placeholder= 'Teu Login'
+            placeholder= 'Login'
             placeholderTextColor={'#808080'}
             value={username}
             onChangeText = {setUsername}
             />
             <TextInput
-            style= {styles.input}
+            style= {styles.input2}
             placeholder= 'Senha'
             placeholderTextColor={'#808080'}
             value={password}
             onChangeText = {setPassword}
             />
             <TouchableOpacity onPress={SignIn} style={styles.button}>
-                <Text style={styles.txtbutton}>Botão Brabo!!</Text>
+                <Text style={styles.txtbutton}>Continuar com o Email</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={Cadastrar} style={styles.button}>
+                <Text style={styles.txtbutton}>Cadastrar-se</Text>
             </TouchableOpacity>
         </View>
     )
@@ -46,21 +54,31 @@ export default function Login({navigation}){
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#6959CD',
+        backgroundColor: '#FFF',
         alignItems: 'center',
         justifyContent: 'space-around',
       },
       text: {
         textAlign: 'center',
-        fontStyle: 'italic',
+        fontWeight: 'bold',
         fontSize: 30,
-        color: '#09427d',
+        color: '#000000	'
+      },
+      text2: {
+        textAlign: 'center',
+        fontSize: 22,
+        color: '#000000	'
+      },
+      text3: {
+        textAlign: 'center',
+        fontSize: 15,
+        color: '#000000	'
       },
       button: {
         shadowRadius: 20,
         borderRadius: 10,
         backgroundColor: '#363636',
-        width: 170,
+        width: 300,
         height: 35,
       },
       txtbutton: {
@@ -70,11 +88,25 @@ const styles = StyleSheet.create({
         textAlign: 'center',
       },
       input:{
-        backgroundColor: '#363636',
+        backgroundColor: '#FFF',
+        borderColor: '#000000',
+        borderWidth: 0.8,
         shadowRadius: 20,
-        borderRadius: 10,
+        borderRadius: 8,
         padding: 10,
+        width: 300,
         height: 37,
+      },
+      input2:{
+        backgroundColor: '#FFF',
+        borderColor: '#000000',
+        borderWidth: 0.8,
+        shadowRadius: 20,
+        borderRadius: 8,
+        padding: 10,
+        width: 300,
+        height: 37,
+
       },
       logo: {
         marginTop: 50,
